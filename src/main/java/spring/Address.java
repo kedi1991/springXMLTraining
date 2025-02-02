a7814a4adbe12cae3fec3a4775e8aa5eac24e873
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-02-02 03:01:12 +0300

    added an address class

---
+++ src/main/java/spring/Address.java
@@ -0,0 +1,49 @@
+package spring;
+
+public class Address {
+
+	String region;
+	
+	String city;
+	
+	int zipCode;
+	
+
+	public Address(String region, String city, int zipCode) {
+		super();
+		this.region = region;
+		this.city = city;
+		this.zipCode = zipCode;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [region=" + region + ", city=" + city + ", zipCode=" + zipCode + "]";
+	}
+	
+	
+}
