9c60ec685f329f474c51a14d421f305159f07076
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-02-02 03:17:32 +0300

    modify the employee class with addredd dependency

--- src/main/java/spring/Employee.java
+++ src/main/java/spring/Employee.java
@@ -5,39 +5,58 @@ public class Employee {
 	int id;
 	String course;
 	
+	Address address;
 	
-
 	public Employee() {
 
 	}
-	public Employee(String name, int id, String course) {
+
+	public Employee(String name, int id, String course, Address address) {
 		super();
 		this.name = name;
 		this.id = id;
 		this.course = course;
+		this.address = address;
 	}
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", id=" + id + ", course=" + course + "]";
-	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getCourse() {
 		return course;
 	}
+
 	public void setCourse(String course) {
 		this.course = course;
 	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + ", course=" + course + ", address=" + address + "]";
+	}
+	
+	
 	
 	
 }
