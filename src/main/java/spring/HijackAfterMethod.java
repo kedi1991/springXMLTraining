77a081e559c7eaa87522834929f023dc26f834cf
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-02-06 22:12:43 +0300

    create the proxy bean for the highjack method class

--- src/main/java/spring/HijackAfterMethod.java
+++ src/main/java/spring/HijackAfterMethod.java
@@ -9,7 +9,7 @@ public class HijackAfterMethod implements AfterReturningAdvice{
 	@Override
 	public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
 		// TODO Auto-generated method stub
-		System.out.println("we are after the point");
+		System.out.println("");
 		
 	}
 
